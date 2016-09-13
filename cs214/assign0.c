3eda3d467c4898f701f3713b28a8daf336f6a3e0
ZACHARY BLANCO <zdb5@DSV60.rutgers.edu>
2016-09-13 18:44:52 -0400

    Moving to binary tree to sort and traverse

--- cs214/assign0.c
+++ cs214/assign0.c
@@ -2,21 +2,23 @@
 #include <stdlib.h>
 #include <string.h>
 
-typedef struct ListNode {
-  struct ListNode* prev;
-  struct ListNode* next;
+typedef struct BNode {
+  struct BNode* prev;
+  struct BNode* next;
   char* data;
-} ListNode;
+} BNode;
 
-// Define the functions to use
+// Function Declarations - See implementations for documentation
 int getLength(char str[]);
 int isAlpha(char c);
 void printChars(char str[]);
 char* createString(int start, int end, char argv[]);
-void printNode(ListNode* ptr);
-ListNode* addToStack(ListNode* stack, char* data);
+void printNode(BNode* ptr);
+void addToTree(BNode* tree, char* data);
+void traverseAndFree(BNode* root);
 
 int main(int argc, char **argv){
+  
   //Print argc, the number of arguments
   printf("Number of arguments: %i\n", argc);
 
@@ -26,34 +28,29 @@ int main(int argc, char **argv){
     return 1;
   }
 
-  ListNode* stack = NULL;
 
+  BNode* tree = NULL;
   int iter = 0; // The iterator variable
   int start = 0, end = 0; // index of a word's last character
 
   //Main loop to iterate over the string
   while (argv[1][iter] != '\0') {
-
     if (!isAlpha(argv[1][iter])){
-
       //We've approached the end of a word
       //Set the end index to the current iterator
       end = iter;
 
       //Ensure end is greater than start - if so then attempt to create the string pointer
       if (end > start) {
-
         char* str = createString(start, end, argv[1]);
-        stack = addToStack(stack, str);
-
+        addToTree(tree, str);
       } else {
         //Index bounds were <= 0 - can't create string from this
         //printf("Bad Bounds. Start: %i , End: %i \n", start, end); // Bounds are not correct    
       }
-      start = iter + 1;
-    
-    
+      start = iter + 1; // Set starting character of the next word to be the next char
     }
+    
     iter++;
   }
 
@@ -61,24 +58,26 @@ int main(int argc, char **argv){
   end = iter;
   if (end > start) {
     char* str = createString(start, end, argv[1]);
-    stack = addToStack(stack, str);
+    addToTree(tree, str);
   }
 
-// Run through the stack (for testing)
-  ListNode* cur = stack;
+// Run through the tree (for testing)
+  BNode* cur = tree;
   int i = 0;
   while ( cur != NULL && i < 15) {
-    printf("Top of Stack: ");
+    printf("Top of Tree: ");
     printf(" %s\n", cur->data);
     //printNode(cur);
     cur = cur->prev;
     i++;
   }
   
+  printf("TRAVERSING:\n");
+  traverseAndFree(tree);
   return 0;
 }
 
-void printNode(ListNode* nodePtr){
+void printNode(BNode* nodePtr){
   if (nodePtr != NULL) {
     printf("NodePtr: %p; PrevPtr: %p; NextPtr: %p; Data: %s\n", nodePtr, (*nodePtr).prev, (*nodePtr).next, (*nodePtr).data);
   } else {
@@ -86,32 +85,58 @@ void printNode(ListNode* nodePtr){
   }
 }
 
+void traverseAndFree(BNode* root) {
+  if (root == NULL){
+    return;
+  }
+  
+  traverseAndFree(root->prev);
+  traverseAndFree(root->next);
+  printf("%s", root->data);
+  free(root->data);
+}
+
 // Add a string to the top of the stack - return the new top of the stack.
 // ARGUMENTS:
 //    stack: The original stack pointer
 //    data: data to add to the stack
 // RETURNS:
-//    ListNode*: A pointer to a new ListNode* which now resides at the top of the stack.
-ListNode* addToStack(ListNode* stack, char* data) {
-  if (stack == NULL) {
-    stack = malloc(sizeof(ListNode));
-    stack->prev = NULL;
-    stack->next = NULL;
-    stack->data = data;
-//    printf("Current: ");
-//    printNode(stack);
+//    BNode*: A pointer to the root BNode* which now resides at the top of the stack.
+void addToTree(BNode* root, char* data) {
+  if (root == NULL) {
+    root = (BNode*) malloc(sizeof(BNode));
+    root->prev = NULL;
+    root->next = NULL;
+    root->data = data;
   } else {
-    //Add the string onto the stack
-    ListNode* newPtr = malloc(sizeof(ListNode));
-    newPtr->prev = stack;
+    BNode* newPtr = (BNode*)malloc(sizeof(BNode));
+    newPtr->prev = NULL;
     newPtr->next = NULL;
     newPtr->data = data;
-    stack->next = newPtr;
-//    printf("Current: ");
-//   printNode(stack);
-    stack = newPtr;
+    //Binary Tree Insert O(n)
+    BNode* cur = root;
+    BNode* prev = root;
+    int cmp;
+    while (cur != NULL) {
+      cmp = strcmp(data, cur->data);
+      prev = cur;
+      if (cmp < 0) {
+        cur = cur->prev;
+      } else if (cmp >= 0) {
+        cur = cur->next;
+      }
+    }
+    //cur was null so we refer to previous pointer to tell us the node where we dropped
+    //set the left/right pointer according to cmp val
+    if (cmp < 0) {
+      prev->prev = newPtr;
+    } else {
+      prev->next = newPtr;
+    }
+    printf("Current: ");
+    printNode(root);
+
   }
-    return stack;
 }
 
 
@@ -139,6 +164,8 @@ int getLength(char str[]) {
   return iter;
 }
 
+void freeNodeMem() {}
+
 //Return a pointer to a character array of a string
 // ARGUMENTS:
 //    start: the start position of the array for the string
@@ -149,7 +176,7 @@ int getLength(char str[]) {
 char* createString(int start, int end, char argv[]){  
   int length = end - start;
   length += 1; //+1 For null terminator
-  char* szPtr = malloc(sizeof(char)*length);
+  char* szPtr = (char*)malloc(sizeof(char)*length);
   int i; 
   for(i = start; i <= end; i++) {
     szPtr[i - start] = argv[i];
@@ -158,7 +185,6 @@ char* createString(int start, int end, char argv[]){
   //printf("START: %i, END: %i\n", start, end);
   //printf("CREATE STRING: \"%s\"; Pointer: %p \n", szPtr, szPtr); //Print string for testing
   return szPtr;
-
 }
 
 //Determine if a character is in the Alphabet or not
@@ -174,21 +200,6 @@ int isAlpha(char c){
   } else {
     return 0;
   }
-}
-
-
-//Compares two strings
-// ARGUMENTS:
-//    first: The first character array
-//    second: The second character array
-// RETURNS:
-//    -1: When first is less than (comes before) second in alphabetic ordering
-//    0:  When both appear at the same location in alphabetic ordering
-//    1:  When first appears after (comes after) second in alphabetic ordering
-// SPECIAL NOTE:
-//    In cases like the two words "bring" and "bringing", because bring is shorter but the first 5 characters are the same
-//    we will put "bring" before "bringing" in the ordering, thus the result would be "-1" if we called compareStr("bring", "bringing")
-int compareStr(char* first, char* second) {
 }
 
 
