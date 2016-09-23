34ed110341208b84007dce8f34ae506cdc4d366c
Zac Blanco <zblanco@apache.org>
2016-09-23 12:56:29 -0400

    [CS214] - Add in initial assignment 0.5 for EC

---
+++ cs214/Asst0-5/part1_functions.h
@@ -0,0 +1,26 @@
+
+
+#ifndef __PART_1_FUNCTIONS_H__
+#define __PART_1_FUNCTIONS_H__
+
+void one(const int a, const int b);
+void two(const char *grade);
+void three();
+void four(const float value);
+void five(const int *x, const int *y);
+float *six(const int *x);
+void seven(const char *a);
+void eight();
+void nine();
+void ten(int *x);
+void eleven(const char *s);
+void twelve();
+void thirteen(int a);
+void fourteen(const char *s);
+void fifteen(const int value);
+char *sixteen();
+void seventeen(const int d);
+void eighteen(int k);
+long int clear_bits(long int value, long int flag);
+
+#endif // __PART_1_FUNCTIONS_H__
