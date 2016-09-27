7c52e2da8117b88fe8b5c4bc3c9c719677c0fa2d
Zac Blanco <zblanco@apache.org>
2016-09-26 22:15:43 -0400

    [CS214] - Asst1: Add bucketing values

--- cs214/Asst1/mymalloc.c
+++ cs214/Asst1/mymalloc.c
@@ -3,6 +3,15 @@
 #include "mymalloc.h"
 
 static char memblock[5000];
+static char allocated[5000];
+static int count = 0;
+
+// A nice round number to use for bucketing algorithm
+// Downfall is that we can't really allocate > 625 bytes
+static int buckets = 8;
+static int c_bucket = 0; // Current bucket
+
+
 
 char *mymalloc(int size, char* file, int line) {
   
