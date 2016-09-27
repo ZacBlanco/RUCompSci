75c8575fe0e8b870bdf2cceb1f283a6913cb6cb4
Mike Botti <mikexbotti@gmail.com>
2016-09-26 22:07:19 -0400

    added static variables in mymalloc

--- cs214/Asst1/test_malloc.c
+++ cs214/Asst1/test_malloc.c
@@ -7,7 +7,15 @@
 int main() {
 	printf("Running Tests\n");
 
+
 	printf("Test Print\n");
+
+    char *test = (char *)malloc(5);
+    printf("Current memory address after using mymalloc: %p\n", test);
+    test = "help";
+    printf("Test: %s\n", test);
+    printf("Test ptr: %p\n", test);
+    
   
   return 0;
   
