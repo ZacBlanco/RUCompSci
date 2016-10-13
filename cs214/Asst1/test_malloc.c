17d30904bc1e105eb6743ace6e7ff0811812a2ad
Zac Blanco <zblanco@apache.org>
2016-10-13 16:00:58 -0400

    [CS214] - Asst1: Test malloc unit testing
    Added some functions in order to run unit tests on code.
    
    1.  assert(int condition, char* str); - A method which lets us check a
    condition and print a message if the condition isn't true. Also sets the
    FAILED flag to true.
    2. Println - print a message with a newline character
    3. pass_msg - pass the name of a test to this message
    4. fail(msg) - prints a message and sets the failed flag to true if the
    condition evaluated to false.

--- cs214/Asst1/test_malloc.c
+++ cs214/Asst1/test_malloc.c
@@ -1,43 +1,124 @@
-#include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
 #include "mymalloc.h"
+
+void fail(char* msg);
+void assert(int condition, char* msg);
+void println(char* msg);
+void pass_msg(char* msg);
+
+void TestBasicMalloc();
+void TestOverMalloc();
+void TestBasicFree();
+
+static int FAILED = 0;
+
 
 
 
 int main() {
 	printf("Running Tests\n");
-  char* a = (char*)malloc(sizeof(char)*5);
-  printf("Ptr1: %p\n", a);
-  int i;
-  for(i = 0; i < 5; i++) {
-    a[i] = 'x';
-  }
-  char b[]  = "help\0";
-  a[0] = b[0];
-  a[1] = b[1];
-  //a[2] = b[5];
-  printf("A: %s\n", a);
-  printf("Ptr2: %p\n", a);
-  char *c = (char *)malloc(sizeof(char) * 10);
-  char *d = (char *)malloc(sizeof(char) * 10);
-  char *e = (char *)malloc(sizeof(char) * 10);
-  char *f = (char *)malloc(sizeof(char) * 10);
-  char *g = (char *)malloc(sizeof(char) * 10);
-  char *h = (char *)malloc(sizeof(char) * 10);
-  char *k = (char *)malloc(sizeof(char) * 10);
-  char *j = (char *)malloc(sizeof(char) * 10);
-  free(a);
-  free(c);
-  free(d);
-  free(e);
-  free(f);
-  free(g);
-  free(h);
-  free(k);
-  free(j);
+
+  println("=============================================");
+  FAILED = 0;
+  TestBasicMalloc();
+  pass_msg("TestBasicMalloc");
+  println("=============================================");
+  
+  
+  println("=============================================");
+  FAILED = 0;
+  TestBasicFree();
+  pass_msg("TestBasicFree");
+  println("=============================================");
+  
+  
+  println("=============================================");
+  FAILED = 0;
+  TestOverMalloc();
+  pass_msg("TestOverMalloc");
+  println("=============================================");
 
   return 0;
   
   
+}
+
+//Test a single malloc call
+// Make sure 2 consecutive bytes have pointers of distance 625
+void TestBasicMalloc() {
+  
+  char* a = malloc(sizeof(char));
+  char* b = malloc(sizeof(char));
+
+  int space = (b) - (a);
+  assert(a != NULL, "a should not be null");
+  assert(b != NULL, "b should not be null");
+  assert(b - a == 500, "Pointers not spaced by 500 bytes");
+
+  char c = 'a';
+  char d = 'b';
+
+  *a = c;
+  *b = d;
+  assert(*a == 'a', "Char does not hold the correct value");
+
+  // printf("Bytes Malloc'd: %i\n", get_bytes_malloc());
+  free(a);
+  assert(get_bytes_malloc() == 1, "Only 1 byte should be malloc'd");
+
+  free(b);
+  assert(get_bytes_malloc() == 0, "No bytes should be malloc'd");
+
+}
+
+void TestBasicFree(){
+  char* a = malloc(sizeof(char));
+  *a = 'b';
+  free(a);
+  assert(*a == '\0', "Memory not nulled on free");
+  assert(get_bytes_malloc() == 0, "No bytes should be malloc'd");
+}
+
+void TestOverMalloc(){
+  
+  char* a = malloc(sizeof(char)*2501);
+  assert(a == NULL, "Malloc of 2501 bytes should be null");
+  assert(get_bytes_malloc() == 0, "No bytes should be malloc'd");
+  
+  a = malloc(sizeof(char)*2500);
+  assert(a != NULL, "Malloc of 2500 bytes should pass");
+
+  char* b = malloc(sizeof(char));
+  assert(b == NULL, "Should not allocate a byte over 2500");
+
+  free(a);
+  free(b);
+  // printf("BM: %i\n", get_bytes_malloc());
+  assert(get_bytes_malloc() == 0, "No bytes should be malloc'd");
+
+}
+
+
+void fail(char* msg) {
+  printf("FAILED: %s\n", msg);
+}
+
+void println(char* msg) {
+  printf("%s\n", msg);
+}
+
+void assert(int condition, char* msg) {
+  if(!condition) {
+    fail(msg);
+    FAILED = 1;
+  } 
+}
+
+void pass_msg(char* test_name){
+  if(!FAILED) {
+    printf("%s passed \u221A \n", test_name);
+  } else {
+    printf("%s FAILED \u00D7\n", test_name);
+  }
 }
