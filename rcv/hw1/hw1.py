47914fac76b75be87e167658bedaa6b358d63b24
Zac <zacdblanco@gmail.com>
2017-09-12 16:13:38 -0400

    rename hellow to hw1

--- rcv/hw1/hw1.py
+++ rcv/hw1/hw1.py
@@ -55,15 +55,6 @@ def problem3():
 
 # Gather out code in a main() function
 def main():
-    print('Hello there {}'.format(sys.argv[1]))
-    # Command line args are in sys.argv[1], sys.argv[2]
-    # sys.argv[0] is the script name itself and can be ignored
-    img = cv2.imread('testimg.jpg',1)
-    # Display with matplotlib
-    plt.imshow(img, interpolation='bicubic')
-    plt.xticks([])
-    plt.yticks([]) # to hide tick values on X and Y axis
-    plt.show()
     print("The matrix has 2 linearly independent columns: {}".format(problem2()))
     problem3()
     # Close the window will exit the program
