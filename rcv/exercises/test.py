33b9883e1b1c24c50d9385b3ed4c10f414aa0d2e
Zac <zacdblanco@gmail.com>
2017-09-25 12:04:58 -0400

    Add files with functions from homeworks 1 and 2

--- rcv/exercises/test.py
+++ rcv/exercises/test.py
@@ -5,11 +5,8 @@ from matplotlib import pyplot as plt
 
 def show_image(filename):
     img = cv2.imread(filename, 1)
-    plt.figure()
-    plt.imshow(img, interpolation='bicubic')
-    plt.xticks([])
-    plt.yticks([])
-    plt.show()
+    cv2.waitKey(0)
+    cv2.imshow("img", image)
     cv2.destroyAllWindows()
 
 def disp_mat(data):
@@ -17,31 +14,54 @@ def disp_mat(data):
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
-def resize_img(img_matrix, dims):
+def svd_compress(img_matrix, dims):
     U, S, V = np.linalg.svd(img_matrix, True)
-    U = U[:,:dims]
+    U = U[,:dims]
     S = np.diag(S)[:dims,:dims]
     V = V[:dims,]
     return U.dot(S).dot(V)
 
-def main():
-    # show_image('baby-monkey.jpg')
-    d = cv2.imread('baby-monkey.jpg', False)
-    U, S, V = np.linalg.svd(d)
-    disp_mat(d)
-    i = resize_img(d, 20)
-    disp_mat(i)
-    disp_mat(d-i)
-    i = resize_img(d, 40)
-    disp_mat(i)
-    i = resize_img(d, 80)
-    disp_mat(i)
-    i = resize_img(d, 200)
-    disp_mat(i)
 
+def warp(image, refPts, mapPts):
+    def warp(image, orig, mapped):
+	'''Warps the image via estimating the 2D homography.
 
+	User must select the points in the following order to warp the billboard with the same
+	perspective.
 
+	- Top Left
+	- Top Right
+	- Bottom Left
+	- Bottom Right
+	'''
+	# Estimate the homography
+	Amat = []
+	for i in range(len(orig)):
+		rp = orig[i]
+		hp = mapped[i]
+		Amat.append([-hp[0], -hp[1], -1, 0, 0, 0, rp[0]*hp[0], rp[0]*hp[1], rp[0]])
+		Amat.append([0, 0, 0, -hp[0], -hp[1], -1, rp[1]*hp[0], hp[1]*rp[1], rp[1]])
+	Amat = np.matrix(Amat)
+	# Get the homography from the null space of A (last col of V)
+	u, s, v = np.linalg.svd(Amat)
+	v = v.H # The V returned is really V.T
+	hom = v[:,-1] # equivalent to 
+	hom.shape = (3, 3) # Reshape back into the 3x3
 
+	# numpy arrays for mappings
+	mapx = np.zeros(image.shape[:2], np.float32)
+	mapy = np.zeros(image.shape[:2], np.float32)
 
-if __name__ == "__main__":
-    main()
+	# Calculate the mapping using the homography matrix
+	for y in range(image.shape[0]): # rows
+		for x in range(image.shape[1]): # cols
+			vec = np.array([x, y, 1]).T
+			pts = hom.dot(vec).T
+			pts = pts/pts[2]
+			mapx[y, x] = pts[0]
+			mapy[y, x] = pts[1]
+	
+	# performs the remapping and display the new image
+	I2 = np.zeros(image.shape, np.float32)
+	I2 = cv2.remap(image, mapx, mapy, 1)
+    return I2
---
+++ rcv/exercises/zrcv.py
@@ -0,0 +1,63 @@
+import sys
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+def show_image(filename):
+    img = cv2.imread(filename, 1)
+    cv2.waitKey(0)
+    cv2.imshow("img", image)
+    cv2.destroyAllWindows()
+
+def disp_matrix(data, key, width=None, height=None):
+	cv2.imshow('img', data)
+	if width != None and height != None:
+		cv2.resizeWindow('img', int(width), int(height))
+	while True:
+		k = cv2.waitKey(50) & 0xFF
+		if k == ord(key):
+			break
+	cv2.destroyAllWindows()
+
+def svd_compress(img_matrix, dims):
+    U, S, V = np.linalg.svd(img_matrix, True)
+    U = U[,:dims]
+    S = np.diag(S)[:dims,:dims]
+    V = V[:dims,]
+    return U.dot(S).dot(V)
+
+
+def warp(image, orig, mapped):
+    '''Warps the image via estimating the 2D homography.
+    '''
+    # Estimate the homography
+    Amat = []
+    for i in range(len(orig)):
+        rp = orig[i]
+        hp = mapped[i]
+        Amat.append([-hp[0], -hp[1], -1, 0, 0, 0, rp[0]*hp[0], rp[0]*hp[1], rp[0]])
+        Amat.append([0, 0, 0, -hp[0], -hp[1], -1, rp[1]*hp[0], hp[1]*rp[1], rp[1]])
+    Amat = np.matrix(Amat)
+    # Get the homography from the null space of A (last col of V)
+    u, s, v = np.linalg.svd(Amat)
+    v = v.H # The V returned is really V.T
+    hom = v[:,-1] # equivalent to 
+    hom.shape = (3, 3) # Reshape back into the 3x3
+
+    # numpy arrays for mappings
+    mapx = np.zeros(image.shape[:2], np.float32)
+    mapy = np.zeros(image.shape[:2], np.float32)
+
+    # Calculate the mapping using the homography matrix
+    for y in range(image.shape[0]): # rows
+        for x in range(image.shape[1]): # cols
+            vec = np.array([x, y, 1]).T
+            pts = hom.dot(vec).T
+            pts = pts/pts[2]
+            mapx[y, x] = pts[0]
+            mapy[y, x] = pts[1]
+
+    # performs the remapping and display the new image
+    I2 = np.zeros(image.shape, np.float32)
+    I2 = cv2.remap(image, mapx, mapy, 1)
+    return I2
